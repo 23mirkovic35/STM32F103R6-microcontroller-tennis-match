d8441cd58ef3841709312cc31e1bf1ef78f43d5e
23mirkovic35 <miroslavmirkovic99@gmail.com>
2022-02-21 10:08:39 +0100

    first commit

---
+++ cubemx/code/Core/Inc/display.h
@@ -0,0 +1,19 @@
+/*
+ * display.h
+ *
+ *  Created on: Jan 31, 2022
+ *      Author: Miroslav
+ */
+
+#ifndef CORE_INC_DISPLAY_H_
+#define CORE_INC_DISPLAY_H_
+
+#include "main.h"
+#include "tim.h"
+#include "gpio.h"
+
+
+extern void display();
+
+
+#endif /* CORE_INC_DISPLAY_H_ */
